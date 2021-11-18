007af958ac59a8a1e46ed9fb209b5c961b0275f3
Luk <Luk.Schm@web.de>
2021-11-18 12:45:55 +0100

    Updated dependencies and used channels for communication

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/MrWong99/IcfgO
 
 go 1.17
 
-require github.com/MrWong99/filemanager v1.0.0
+require github.com/MrWong99/filemanager v1.0.1
--- main.go
+++ main.go
@@ -49,6 +49,36 @@ func main() {
 	var readers []Reader = []Reader{consoleIO, &inFile}
 	var writers []Writer = []Writer{consoleIO, &outFile}
 
+	// Initialize a slice of channels that can be used to send slices of strings
+	var scrambleChannels []chan []string
+
+	for idx, w := range writers {
+		// For each writer add a new channel to the list of channels which awaits exactly one string slice
+		scrambleChannels = append(scrambleChannels, make(chan []string, 1))
+
+		wg.Add(1) // This tells the wait group that there is a new routine started
+
+		// Call a new async go routine that outputs to the writer.
+		// The writer and the channel have to be specifically given as function paramerter
+		// since their references "w" and "scramblechannels[idx]" are changed for each iteration in the loop
+		// which would result in unexpected/unwanted behaviour.
+		// The type syntax "<-chan []string" indicates that the given channel can only be used to read
+		// messages but not send
+		go func(writy Writer, inputChan <-chan []string) {
+			defer wg.Done() // Upon exiting the routine tell the wait group that one routine is finished
+
+			// Wait for a message to be received on the given channel. This will block this go routine until this happens
+			scrambledOutput := <-inputChan
+
+			log.Printf("Started writing output to %T\n", writy)
+			err := writy.Write(scrambledOutput)
+			if err != nil {
+				log.Printf(`Error while processing write to "%T": %v`+"\n", writy, err)
+			}
+			log.Printf("Finished writing output to %T\n", writy)
+		}(w, scrambleChannels[idx])
+	}
+
 	// Printf with %#v placeholders is good for slices. See https://yourbasic.org/golang/fmt-printf-reference-cheat-sheet/
 	log.Printf("Scramble adventure! Using %#v as input to scramble and saving output to %#v\n", readers, writers)
 
@@ -64,21 +94,15 @@ func main() {
 
 	scrambledOutput := scramble(scramblePreparation)
 
-	for _, w := range writers {
-		wg.Add(1) // This tells the wait group that there is a new routine started
-
-		// Call a new async go routine that outputs to the writer.
-		// The writer has to be specifically given as function paramerter since the reference "w" is changed for each iteration in the loop
-		// which would result in unexpected/unwanted behaviour
-		go func(writy Writer) {
-			defer wg.Done() // Upon exiting the routine tell the wait group that one routine is finished
-			log.Printf("Started writing output to %T\n", writy)
-			err := writy.Write(scrambledOutput)
-			if err != nil {
-				log.Printf(`Error while processing write to "%T": %v`+"\n", writy, err)
-			}
-			log.Printf("Finished writing output to %T\n", w)
-		}(w)
+	// Send the scrambled output via the previously created channels to all writers.
+	// Start a go routine so it is non-blocking
+	for _, outputChan := range scrambleChannels {
+		// Initialize a new go routine and give the channel as function parameter since we are in a loop (same as above)
+		// The type syntax "chan<- []string" indicates that the given channel can only be used to send
+		// messages but not read
+		go func(outChan chan<- []string) {
+			outChan <- scrambledOutput
+		}(outputChan)
 	}
 
 	wg.Wait() // Wait for all routines to finish
